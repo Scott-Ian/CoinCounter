41cd472a2f119084e13fb727b57ab0309ef8ba33
Ian Scott <chesnekov@gmail.com>
2020-08-31 10:15:58 -0700

    add second successfully failing test if input is NaN
    Co-authored-by: MarielHamson <mariel.hamson@gmail.com>
    Co-authored-by: Fatma Dogan <fceyda@gmail.com>

--- __tests__/CoinCounter.test.js
+++ __tests__/CoinCounter.test.js
@@ -3,10 +3,16 @@ import { CoinCounter } from './../src/CoinCounter'
 describe('change', () => {
 
   test('should take in a decimal value and return a string', () => {
-    expect(CoinCounter()).toBeDefined();
+    expect(CoinCounter(4.99)).toBeDefined();
   });
 
+  test('should return an error message if argument is not a number', () => {
+    expect(CoinCounter("tree").toEqual("invalid input"));
+  });
 
 });
+
+
+
 // Test logic!
 // Probably want you import something you're using in the project-name.js
